ff086ea07b7d6608314448124d2abb64b42d1360
bulletmys <bulletmys@yandex.ru>
2019-10-11 21:39:30 +0300

    Test Travis CI10

---
+++ CMakeFiles/techpatk_cpp_dz1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/techpatk_cpp_dz1.dir/main.c.o"
+  "techpatk_cpp_dz1"
+  "techpatk_cpp_dz1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/techpatk_cpp_dz1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/techpatk_cpp_dz1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for techpatk_cpp_dz1.
+# This may be replaced when dependencies are built.
