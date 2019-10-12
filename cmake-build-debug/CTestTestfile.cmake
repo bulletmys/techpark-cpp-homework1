055fb902ce97d4363e642333d9a19472439830a1
bulletmys <bulletmys@yandex.ru>
2019-10-12 03:29:38 +0300

    test5

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/bulletmys/CLionProjects/techpatk-cpp-dz1
+# Build directory: /home/bulletmys/CLionProjects/techpatk-cpp-dz1/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(SortByDensity "SortByDensity")
+set_tests_properties(SortByDensity PROPERTIES  _BACKTRACE_TRIPLES "/home/bulletmys/CLionProjects/techpatk-cpp-dz1/CMakeLists.txt;13;add_test;/home/bulletmys/CLionProjects/techpatk-cpp-dz1/CMakeLists.txt;0;")
+subdirs("lib/googletest-master")
