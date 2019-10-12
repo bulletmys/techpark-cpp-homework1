06b7683c7405cf407618006e9b6a0c98f5237a4b
bulletmys <bulletmys@yandex.ru>
2019-10-12 11:41:31 +0300

    res

--- main.c
+++ main.c
@@ -35,8 +35,8 @@ char *Input_string() {
                 return NULL;
             }
             if (buf.string) {
-                //tmp = strcpy(tmp, buf.string);
-                tmp = memcpy(tmp, buf.string, (sizeof(*buf.string)));
+                //tmp = strcpy(tmp, buf.string);1
+                tmp = memcpy(tmp, buf.string, (sizeof(char)*buf.size));
                 free(buf.string);
             }
             buf.string = tmp;
@@ -66,17 +66,17 @@ int Input_int() {
     return result;
 }
 
-//int Compare(const void *x1, const void *x2) { Компаратор для qsort
-//    float p_1 = (float) (*(Country *) x1).population / (*(Country *) x1).square;
-//    float p_2 = (float) (*(Country *) x2).population / (*(Country *) x2).square;
-//
-//    if (p_1 > p_2) {
-//        return 1;
-//    } else if (p_1 < p_2) {
-//        return -1;
-//    }
-//    return 0;
-//}
+int Compare(const void *x1, const void *x2) { //Компаратор для qsort
+    float p_1 = (float) (*(Country *) x1).population / (*(Country *) x1).square;
+    float p_2 = (float) (*(Country *) x2).population / (*(Country *) x2).square;
+
+    if (p_1 > p_2) {
+        return 1;
+    } else if (p_1 < p_2) {
+        return -1;
+    }
+    return 0;
+}
 
 void sortAndPrint(Country country[], int N) {
     Country tmp;
@@ -111,7 +111,7 @@ void PrintCountries(Country *countries, int num_of_countries, int start) {
                countries[i].capital,
                countries[i].population,
                countries[i].square,
-               countries[i].population / countries[i].square);
+               countries[i].density);
     }
     printf("\n");
 }
@@ -133,6 +133,7 @@ Country *AddCountry(Country *countries, int *cur_num) {
         countries[i].population = Input_int();
         printf("Enter the area of the country (in sq. Km.)\n");
         countries[i].square = Input_int();
+        countries[i].density = countries[i].population / countries[i].square;
     }
 
     printf("\nNew data added:\n");
